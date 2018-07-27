529ce8920427636dba470dac2bead37b6f4de766
Waliyullah Ayomide Roji <haryomide@localhost.localdomain>
2018-07-27 22:57:53 +0100

    Modified Login Page

--- views/login.php
+++ views/login.php
@@ -70,7 +70,7 @@
                             data: {email: username, password: password},
                             cache: false,
                             success: function (data) {
-                                window.location = "http://arm";
+                                window.location = "http://apartment-rental-mgt";
                             },
                             error: function () {}
                         });
